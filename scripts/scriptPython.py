e06b9d4e4e2acdf09c91e19781b1683b6a585f3f
soufianeAourinmouche <soufiane.aourinmouche@symphony.com>
2020-01-28 10:51:42 +0100

    adding script to retrieve commits with tokens env + variables

---
+++ scripts/scriptPython.py
@@ -0,0 +1,10 @@
+import json
+
+tokens = ['environment', 'variable', 'env', 'var']
+
+with open('jsons/thinksBoard.json ', 'r') as f:
+    logs_json = json.load(f)
+
+for log in logs_json :
+	if any(token in log['subject'] for token in tokens) :
+		print(log['subject']+ "   :   " + log['sha'])
