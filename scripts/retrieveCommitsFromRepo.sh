a6ff49256c36f7f17b1fbceb442d793f2cdc3ab1
enaifuos <sportaouri@gmail.com>
2020-02-09 16:38:05 +0100

    retrieveCommitsFromRepo.sh takes the repository path to analyse in arguments

--- scripts/retrieveCommitsFromRepo.sh
+++ scripts/retrieveCommitsFromRepo.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-./gitjson log --json=haeds > commits.json
+
+USAGE='''
+	This script is used to retrieve all commits from a repository.
+	It uses gitjson script.
+
+	The output JSON file containing commits will be stored in a JSON file with the name of the repository
+	
+	Example :
+
+	RUN : ./retrieveCommitsFromRepo.sh ~/Desktop/repositories/repository/to/analyse
+	OUTPUT : analyse.json
+
+'''
+if [ $# -eq 1 ]
+then
+	echo "Retrieving commits from the repository $1"
+	output_file_name=$(basename "$1")
+	output_file_name+=".json"
+
+	echo "Output will be stored in $output_file_name"
+
+	./gitjson log --json=haeds --repo=$1 > $output_file_name
+else
+	echo "Give repository path as argument :\n RUN : ./retrieveCommitsFromRepo.sh ~/Desktop/repositories/repository/to/analyse"
+fi
