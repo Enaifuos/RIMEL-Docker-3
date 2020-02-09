446e3ddf945b9be5c04f954f97408faac2d22336
enaifuos <sportaouri@gmail.com>
2020-02-09 17:14:57 +0100

    retrieve commits and filter commits scripts take output file in argument

--- scripts/retrieveCommitsFromRepo.sh
+++ scripts/retrieveCommitsFromRepo.sh
@@ -4,23 +4,20 @@ USAGE='''
 	This script is used to retrieve all commits from a repository.
 	It uses gitjson script.
 
-	The output JSON file containing commits will be stored in a JSON file with the name of the repository
+	The output JSON file containing commits will be stored in a JSON file given in argument.
 	
 	Example :
 
-	RUN : ./retrieveCommitsFromRepo.sh ~/Desktop/repositories/repository/to/analyse
+	RUN : ./retrieveCommitsFromRepo.sh ~/Desktop/repositories/repository/to/analyse outputFileName.json
 	OUTPUT : analyse.json
 
 '''
-if [ $# -eq 1 ]
+if [ $# -eq 2 ]
 then
 	echo "Retrieving commits from the repository $1"
-	output_file_name=$(basename "$1")
-	output_file_name+=".json"
+	echo "Output will be stored in $2"
 
-	echo "Output will be stored in $output_file_name"
-
-	./gitjson log --json=haeds --repo=$1 > $output_file_name
+	./gitjson log --json=haeds --repo=$1 > $2
 else
-	echo "Give repository path as argument :\n RUN : ./retrieveCommitsFromRepo.sh ~/Desktop/repositories/repository/to/analyse"
+	echo "Give repository path as argument :\n RUN : ./retrieveCommitsFromRepo.sh ~/Desktop/repositories/repository/to/analyse outputFileName.json"
 fi
