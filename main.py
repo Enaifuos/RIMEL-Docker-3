6a936c1818faef03349e9f7b162241603dcc7db3
Walid LARABI <walid.larabi@etu.unice.fr>
2020-03-04 15:41:58 +0100

    analysing time

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
+import time
+
+
 from scripts.utils import getAllEVfromRepo, getCommitsWhereKeywordsAppear, getAllCommitsJSONFormat, getPreviousAndNext, \
     getFilesAndMethodsModified
 
 print("---------------------------------------")
 cwd = os.getcwd()
+start_time = time.time()
 print("initial cwd : \n" + cwd)
 print("---------------------------------------")
 
@@ -29,28 +33,29 @@ print("---------------------------------------")
 
 REPO = "/home/passport/Repos/tmp/thingsboard"
 
-
 print(" 1/6  -  Get VE from Project")
 EVs = getAllEVfromRepo(REPO)
+print("--- %s seconds ---" % (time.time() - start_time))
 # print(EVs)
 
 print(" 2/6  -  Get all commits in json format")
 allCommitsJSONFormat = getAllCommitsJSONFormat(REPO, "thingsboard.json")
+print("--- %s seconds ---" % (time.time() - start_time))
 # print(allCommitsJSONFormat)
 
 print(" 3/6  -  Get commits where EV appears")
 commitsWhereEVsAppears = getCommitsWhereKeywordsAppear(REPO, EVs)
+print("--- %s seconds ---" % (time.time() - start_time))
 # print(commitsWhereEVsAppears)
 
-print(" 4/6  -  Get commits where EV appears")
-commitsWhereEVsAppears = getCommitsWhereKeywordsAppear(REPO, EVs)
-# print(commitsWhereEVsAppears)
-
-print(" 5/6  -  Get previous and next of each filtered commit")
+print(" 4/6  -  Get previous and next of each filtered commit")
 jsonPreviousNextCommit = getPreviousAndNext(allCommitsJSONFormat, commitsWhereEVsAppears)
+print("--- %s seconds ---" % (time.time() - start_time))
 # print (jsonPreviousNextCommit)
 
-print(" 6/6  -  Get files where EV where touched")
+print(" 5/6  -  Get files where EV where touched")
 filesmodified = getFilesAndMethodsModified(jsonPreviousNextCommit, REPO)
 print(filesmodified)
+print("--- %s TOTAL TIME ---" % (time.time() - start_time))
+
 
